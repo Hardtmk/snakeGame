6aa48f1b845b148e8015bae55cd6d1fe1e5865e2
Hardtmk <miu000369@gmail.com>
2022-07-02 11:12:53 +0800

    add server

---
+++ server/backend.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const app=express()
+
+app.use(express.static('public'))
+
+
+app.get('/',(req,res)=>{
+res.sendFile('./page/snake.html',{root:__dirname})
+// res.send('1sdds23')
+})
+
+app.listen(process.env.PORT || 5000,()=>{
+
+ console.log('listening...')
+})
+
